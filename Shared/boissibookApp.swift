b2d6d778af3b8e1667fc40aea51a0bb6cdebacd7
Rémy Machavoine <remygt@hotmail.fr>
2022-06-30 21:17:01 +0200

    init repo

---
+++ Shared/boissibookApp.swift
@@ -0,0 +1,17 @@
+//
+//  boissibookApp.swift
+//  Shared
+//
+//  Created by Rémy Machavoine on 30/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct boissibookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
